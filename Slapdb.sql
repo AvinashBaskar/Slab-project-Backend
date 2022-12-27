8e9ae0ff78da10345a6c9ec40c65669cfe161f44
AvinashBaskar <114015979+AvinashBaskar@users.noreply.github.com>
2022-12-27 13:13:24 +0530

    Add files via upload

---
+++ Slapdb.sql
@@ -0,0 +1,20 @@
+select * from Slaptable
+
+
+
+create table Slaptable(slapid int primary key identity(1,1),
+Slap_name varchar (100),
+Pay_Group_Code varchar(30),
+Slap_Low_value varchar(30),
+Slap_High_value varchar(30),
+Slap_Percentage float,
+Slap_Value varchar(30))
+
+
+create table SlapDefinition(Slid int primary key identity(1,1),
+Short_Name varchar (30),
+Pay_Group_Code varchar(30),
+Description_s varchar(30),
+Status_s varchar(30)
+
+select * from SlapDefinition
